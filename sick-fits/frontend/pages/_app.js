b1a060ac9fd755e6d931d4933a26083ba0daebd5
Andy Wendt <andy@awendt.com>
2019-06-14 18:30:36 -0600

    Add my app custom app component

---
+++ sick-fits/frontend/pages/_app.js
@@ -0,0 +1,15 @@
+import App, {Container} from 'next/app';
+
+class MyApp extends App {
+  render() {
+    const { Component } = this.props;
+
+    return (
+      <Container>
+        <p>I'm on every page</p>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
