046cddda4a1455818dc24d247c2db43a12a9f211
Andy Wendt <andy@awendt.com>
2019-06-14 18:10:14 -0600

    Update index.js to stateless functional component

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-
-class Home extends React.Component {
-  render() {
-    return <p>Hey!!!</p>;
-  }
-}
+const Home = props => (
+  <div>
+    <p>Hey!!!</p>
+  </div>
+);
 
 export default Home;
