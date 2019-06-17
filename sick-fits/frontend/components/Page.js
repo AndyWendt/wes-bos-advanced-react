f1f4c4219f9b1e65b31da3c72ed4e7a09c892e74
Andy Wendt <andy@awendt.com>
2019-06-17 07:57:19 -0600

    Add in meta element

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Meta from './Meta';
 
 export default class Page extends React.Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
         {this.props.children}
       </div>
