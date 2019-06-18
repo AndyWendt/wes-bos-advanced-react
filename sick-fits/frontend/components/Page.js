5f025b436e6edd3bd8d15ac39c2676fbd77880a4
Andy Wendt <andy@awendt.com>
2019-06-18 07:49:15 -0600

    Add styled page and inner component

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default class Page extends React.Component {
   render() {
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <Header />
-        {this.props.children}
-      </div>
+        <Inner>
+          {this.props.children}
+        </Inner>
+      </StyledPage>
     );
   }
 }
