9414c7f5e7c44688cf2480b4561fd234ee0dc30b
Andy Wendt <andy@awendt.com>
2019-06-18 08:09:43 -0600

    Add media query...

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -8,6 +8,17 @@ const Logo = styled.h1`
   position: relative;
   z-index: 2;
   transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
 `;
 
 const Header = () => (
