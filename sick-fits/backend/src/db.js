f284e87b3b65613a5c398a188161534b827aaf57
Andy Wendt <andy@awendt.com>
2019-07-08 08:16:52 -0600

    Add db js

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,8 @@
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
