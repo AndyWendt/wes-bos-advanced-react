3c366974e2dcd610cccf499c8935b01f0b497136
Andy Wendt <andy@awendt.com>
2019-07-08 17:54:42 -0600

    update server start

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -6,4 +6,11 @@ const db = require('./db');
 
 const server = createServer();
 
-
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }
+}, deets => {
+  console.log(`Server is now running on port: ${deets.port}`)
+});
