a609e34996ec15308f4453055c00a2de23b3bd64
Andy Wendt <andy@awendt.com>
2019-07-10 07:45:37 -0600

    Refactor to forwardto

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,7 @@
+const {forwardTo} = require('prisma-binding');
+
 const Query = {
-  async items(parent, args, context, info) {
-    const items = await context.db.query.items();
-    return items;
-  }
+  items: forwardTo('db'),
 };
 
 module.exports = Query;
