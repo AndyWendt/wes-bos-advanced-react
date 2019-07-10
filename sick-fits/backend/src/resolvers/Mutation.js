356a93771aaf6f647ef8caae557059e68851bf46
Andy Wendt <andy@awendt.com>
2019-07-09 18:32:30 -0600

    Fix breaking server

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,4 @@
 const Mutations = {
-  createDog(parent, args, context, info) {
-    global.dogs = global.dogs || [];
-    const newDog = {name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
-  }
 };
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,4 @@
 const Query = {
-  dogs(parent, args, context, info) {
-    global.dogs = global.dogs || [];
-    return global.dogs;
-  }
 };
 
 module.exports = Query;
