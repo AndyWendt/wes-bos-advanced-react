b280db3b0783b579ec6d8b8837c48512b38793ac
Andy Wendt <andy@awendt.com>
2019-07-09 18:43:29 -0600

    Add createItem mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,4 +1,14 @@
 const Mutations = {
+  async createItem(parent, args, context, info) {
+    // passing info makes sure that the actual item is passed back to us
+    const item = await context.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info)
+
+    return item;
+  }
 };
 
 module.exports = Mutations;
