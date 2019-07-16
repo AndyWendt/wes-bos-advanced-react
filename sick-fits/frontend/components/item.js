69a2a97507a7482e62deb1e46b7aa0407d91fe9a
Andy Wendt <andy@awendt.com>
2019-07-15 18:41:19 -0600

    Add price tag

--- sick-fits/frontend/components/item.js
+++ sick-fits/frontend/components/item.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
 
 export default class Items extends Component {
   static propTypes = {
@@ -21,6 +22,7 @@ export default class Items extends Component {
           <a>{item.title}</a>
         </Link>
       </Title>
+      <PriceTag>{formatMoney(item.price)}</PriceTag>
     </ItemStyles>
   }
 }
