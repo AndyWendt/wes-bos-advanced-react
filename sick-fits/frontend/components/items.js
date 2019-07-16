f42db44fc50fdfd9d2db27daf62325d9caae06e2
Andy Wendt <andy@awendt.com>
2019-07-15 18:09:30 -0600

    Have items components displaying from query

--- sick-fits/frontend/components/items.js
+++ sick-fits/frontend/components/items.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
 
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+      items {
+        id
+        title
+        price
+        description
+        image
+        largeImage
+      }
+  }
+`;
+
 export default class Items extends Component {
   render() {
     return (
       <div>
         <p>Items!</p>
+        <Query query={ALL_ITEMS_QUERY}>
+          {(payload) => {
+            console.log(payload);
+            return <p>Child of payload</p>;
+          }}
+        </Query>
       </div>
     )
   }
