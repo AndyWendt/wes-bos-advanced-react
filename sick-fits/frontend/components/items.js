3dde0e8f77625de12915714b2ebd5bdb52f6324b
Andy Wendt <andy@awendt.com>
2019-07-15 18:22:58 -0600

    Add css for items list and convert

--- sick-fits/frontend/components/items.js
+++ sick-fits/frontend/components/items.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -15,6 +16,18 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
 export default class Items extends Component {
   render() {
     return (
@@ -24,7 +37,9 @@ export default class Items extends Component {
           {({data, error, loading}) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
-            return <p>Found {data.items.length} items</p>;
+            return <ItemsList>
+              {data.items.map(item => <p key={item.id}>{item.title}</p>)}
+            </ItemsList>
           }}
         </Query>
       </div>
