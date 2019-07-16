af153a84062b75951d47bfaa8cbff45956480e18
Andy Wendt <andy@awendt.com>
2019-07-16 09:04:15 -0600

    Fixup create item

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import Mutation from 'react-apollo/Mutation';
-import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Router from 'next/router';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 import Error from './ErrorMessage';
 
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
-      $title: String!
-      $description: String!
-      $price: Int!
-      $image: String
-      $largeImage: String
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
   ) {
     createItem(
       title: $title
@@ -20,34 +21,43 @@ const CREATE_ITEM_MUTATION = gql`
       image: $image
       largeImage: $largeImage
     ) {
-    id
-  }
+      id
+    }
   }
 `;
 
 class CreateItem extends Component {
   state = {
-    title: 'Test title',
-    description: 'Test description',
+    title: 'Cool Shoes',
+    description: 'I love those shoes',
     image: 'dog.jpg',
-    largeImage: 'large.jpg',
-    price: 0,
+    largeImage: 'large-dog.jpg',
+    price: 1000,
   };
-
-  handleChange = (event) => {
-    const { name, type, value } = event.target;
+  handleChange = e => {
+    const { name, type, value } = e.target;
     const val = type === 'number' ? parseFloat(value) : value;
-    this.setState({ [name]: val })
+    this.setState({ [name]: val });
   };
 
   render() {
     return (
-      <Mutation query={CREATE_ITEM_MUTATION} variables={this.state}>
-        {(createItem, {loading, error, called, data}) => (
-          <Form onSubmit={async event => {
-            event.preventDefault();
-            const res = await createItem();
-          }}>
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error }) => (
+          <Form
+            onSubmit={async e => {
+              // Stop the form from submitting
+              e.preventDefault();
+              // call the mutation
+              const res = await createItem();
+              // change them to the single item page
+              console.log(res);
+              Router.push({
+                pathname: '/item',
+                query: { id: res.data.createItem.id },
+              });
+            }}
+          >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
               <label htmlFor="title">
@@ -56,44 +66,45 @@ class CreateItem extends Component {
                   type="text"
                   id="title"
                   name="title"
-                  placeholder="title"
+                  placeholder="Title"
                   required
                   value={this.state.title}
                   onChange={this.handleChange}
                 />
               </label>
+
               <label htmlFor="price">
                 Price
                 <input
-                  type="text"
+                  type="number"
                   id="price"
                   name="price"
-                  placeholder="price"
+                  placeholder="Price"
                   required
                   value={this.state.price}
                   onChange={this.handleChange}
                 />
               </label>
+
               <label htmlFor="description">
                 Description
                 <textarea
-                  type="text"
                   id="description"
                   name="description"
-                  placeholder="description"
+                  placeholder="Enter A Description"
                   required
                   value={this.state.description}
                   onChange={this.handleChange}
                 />
               </label>
-              <button type='submit'>Submit</button>
+              <button type="submit">Submit</button>
             </fieldset>
           </Form>
         )}
       </Mutation>
-)
+    );
   }
 }
 
 export default CreateItem;
-export {CREATE_ITEM_MUTATION};
+export { CREATE_ITEM_MUTATION };
