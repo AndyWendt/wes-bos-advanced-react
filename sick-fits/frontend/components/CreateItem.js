e400fe60faf4e2fb36bccd216d9ac4103287939a
Andy Wendt <andy@awendt.com>
2019-07-16 07:31:46 -0600

    Add create item page

---
+++ sick-fits/frontend/components/CreateItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Mutation from 'react-apollo/Mutation';
+import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
+
+class CreateItem extends Component {
+  render() {
+    <Form>
+      <h2>Sell all your items</h2>
+    </Form>
+  }
+}
+
+export default CreateItem;
