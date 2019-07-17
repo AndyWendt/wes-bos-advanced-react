3722a8077d064ad10979e0e79229ec8c3c5adf68
Andy Wendt <andy@awendt.com>
2019-07-17 08:23:25 -0600

    Add image uploads

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -40,6 +40,26 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async event => {
+    console.log('uploading file')
+    const files = event.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits-awendt');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/commandz/image/upload', {
+      'method': 'POST',
+      'body': data,
+    });
+
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -60,6 +80,17 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Title
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+              </label>
               <label htmlFor="title">
                 Title
                 <input
